c0982cf6d856ce8fe66c462686dae66d6bbaa98d
Jamie Bloxham <jamie.a.bloxham@gmail.com>
2018-04-22 22:00:11 -0400

    SOLA uses correlation to find better segments to use- quality is better now

--- pitch_scaling.py
+++ pitch_scaling.py
@@ -1,7 +1,7 @@
 from util import *
 from scipy.io import wavfile
 
-def time_stretch_sola(signal, alpha, window_len = 2048, taper_len = 256):
+def time_stretch_sola(signal, alpha, window_len = 8192, taper_len = 2048, candid_len = 1024):
     """
     Performs time stretching on an audio signal. Uses the
     synchronized overlap-add method (SOLA).
@@ -10,10 +10,13 @@ def time_stretch_sola(signal, alpha, window_len = 2048, taper_len = 256):
     signal: audio signal
     alpha: the factor by which to time stretch
     """
-    windowed_signals = []
-    for i in range(int(len(signal)/window_len * alpha)):
-        offset = int((window_len - taper_len) * i / alpha)
-        windowed_signal = get_windowed_signal(signal, offset, window_len, taper_len)
+    windowed_signals = [get_windowed_signal(signal, 0, window_len, taper_len)]
+    prev_offset = 0
+    for i in range(1, int(len(signal)/(window_len-taper_len) * alpha)):
+        offset = int((window_len- taper_len) * i / alpha)
+        best_offset = get_best_signal_offset(signal, prev_offset, offset - candid_len, offset + candid_len, window_len)
+        prev_offset = best_offset
+        windowed_signal = get_windowed_signal(signal, best_offset, window_len, taper_len)
         windowed_signals += [windowed_signal]
 
     return overlap_add(windowed_signals, window_len, taper_len)
@@ -22,19 +25,20 @@ def get_windowed_signal(signal, t, window_len, taper_len):
     window = np.arange(0, taper_len, 1) * 1./taper_len
     window = np.append(window, np.full(window_len - 2*taper_len, 1))
     window = np.append(window, np.arange(taper_len, 0, -1) * 1./taper_len)
-    return signal[t:t+window_len] * window
+    trunc_signal = signal[t:t+window_len]
+    return trunc_signal * window[:len(trunc_signal)]
 
-def get_best_signal(signal, prev_t, candid_ti, candid_tf, window_len):
-    prev_signal = signal[prev_t:prev_t+window_len]
-    candid_signal = signal[candid_ti:candid_tf+window_len]
+def get_best_signal_offset(signal, prev_offset, candid_offset_low, candid_offset_high, window_len):
+    prev_signal = signal[prev_offset:prev_offset+window_len]
+    candid_signal = signal[candid_offset_low:candid_offset_high+window_len]
     correlation = np.correlate(prev_signal, candid_signal)
-    print correlation
+    return candid_offset_high - np.argmax(correlation)
 
 def overlap_add(signals, window_len, taper_len):
     output = np.zeros(len(signals) * (window_len-taper_len) + taper_len)
     for i, signal in enumerate(signals):
         offset = (window_len - taper_len) * i
-        output[offset:offset+window_len] += signal
+        output[offset:offset+len(signal)] += signal
     return output
 
 def resample(signal, alpha):
@@ -56,7 +60,7 @@ def pitch_scale(fp, alpha):
     return resample(time_stretched_signal, alpha)
 
 if __name__ == "__main__":
-    signal = pitch_scale('samples/3notes_human.wav', 1)
+    signal = pitch_scale('samples/3notes_human.wav', 0.8)
     play_signal(signal)
 
 
